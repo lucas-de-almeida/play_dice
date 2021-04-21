0d36ed96a1ae061795e26d008a85aea6db32531e
Almeida <lalmeida6@PMINTL.NET>
2021-04-21 03:55:47 -0300

    adicionado imagens e função de imagem aleatoria aplicativo funcinal

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,65 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PlayDice());
 }
 
-class MyApp extends StatelessWidget {
+class PlayDice extends StatefulWidget {
+  @override
+  _PlayDiceState createState() => _PlayDiceState();
+}
+
+class _PlayDiceState extends State<PlayDice> {
+  int nextDiceImage = 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: Colors.red,
+          title: Text(
+            'Play Dice',
+          ),
+        ),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Spacer(),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  24,
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/dice/dice-$nextDiceImage.png',
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Spacer(),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white)),
+              onPressed: () {
+                setState(() {
+                  nextDiceImage = Random().nextInt(6) + 1;
+                });
+              },
+              child: Text(
+                'Roll the dice',
+                style: TextStyle(color: Colors.black),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
